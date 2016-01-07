5a575699df94a4d943fa4fb82b92bcbf94a13a6a
lai yao <yao.prower@gmail.com>
2016-01-07 21:59:25 +0800

    做了杨辉三角，棒棒哒

---
+++ PythonApplication1/杨辉三角.py
@@ -0,0 +1,22 @@
+﻿#杨辉三角
+def triangles(l=1):
+    Ltriangles=[1]
+    while len(Ltriangles) <= l:
+        yield([a for a in Ltriangles])
+        for n in range(len(Ltriangles)-1):
+            Ltriangles[n]=Ltriangles[n]+Ltriangles[n+1]
+        Ltriangles.insert(0,1)
+
+
+def triangles2(l=1):
+    b = [1]
+    while True:
+        yield(b)
+        b = [1] + [b[i] + b[i+1] for i in range(len(b)-1)] + [1]
+        if len(b) >= l:
+            return(0)
+
+num=10
+num=int(input('input Number:'))
+a=[x for x in triangles(num)]
+print(a)

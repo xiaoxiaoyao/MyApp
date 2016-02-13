4f4053f6d0748a93e51f62e42886de7286e292c2
lai yao <yao.prower@gmail.com>
2016-02-14 03:04:58 +0800

    async await

--- PythonApplication1/自己的小练习/asynciotest.py
+++ PythonApplication1/自己的小练习/asynciotest.py
@@ -1,24 +1,27 @@
 ﻿import time
 import datetime
 import asyncio
-#在同一个线程里并行执行  异步IO
-@asyncio.coroutine
-def async_now(*args, **kw):
-    print(datetime.datetime.now(),*args)
-    print("Hello world!")    
-    now_a= yield from asyncio.sleep(1)
+#在同一个线程里并行执行，异步IO，python3.5新特性
+
+async def async_now(*args, **kw):
+    print(datetime.datetime.now(),' Hello once! , run now_a= await asyncio.sleep(1) ,async_id=',*args)
+    now_a= await asyncio.sleep(1)
+    print(datetime.datetime.now(),' Hello twice! , run time.sleep(1) ,async_id=',*args)
     time.sleep(1)
-    print(datetime.datetime.now(),*args)
-    print("Hello sleep!")
-    now_a= yield from asyncio.sleep(1)
-    print(now_a)
-    print(datetime.datetime.now(),*args)
-    print("Hello again!",*args)
+    print(datetime.datetime.now(),' Hello thrice! , run again now_a= await asyncio.sleep(1) ,async_id=',*args)
+    now_a= await asyncio.sleep(1)
+    print(datetime.datetime.now(),'hello four times! , now return ,async_id=',*args)
     return
 
 print(datetime.datetime.now())
-print('loop = asyncio.get_event_loop()')
-run=[async_now(x) for x in range(1, 11)]
+print('''
+run 
+
+run=[async_now(x) for x in range(0,7)]
+loop = asyncio.get_event_loop()
+
+''')
+run=[async_now(x) for x in range(0,7)]
 loop = asyncio.get_event_loop()
 loop.run_until_complete(asyncio.wait(run))
 loop.close

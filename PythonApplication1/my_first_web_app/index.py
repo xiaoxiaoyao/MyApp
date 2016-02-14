8145701e03cd8576373dac23682edd9e5623c8c6
lai yao <yao.prower@gmail.com>
2016-02-14 23:19:30 +0800

    简单的WEB页面完成了
    BY aiohttp

--- PythonApplication1/my_first_web_app/index.py
+++ PythonApplication1/my_first_web_app/index.py
@@ -4,17 +4,37 @@ import asyncio, os, json, time
 from datetime import datetime
 from aiohttp import web
 
-def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>')
+###这些全都是异步，aiohttp就是异步，所以要async
+async def hellow_world(request):
+    text=b'<h1>hellow world!</h1>'
+    ###最简单的hellow world!，最基础的return
+    return web.Response(body=text)
 
-@asyncio.coroutine
-def init(loop):
-    app = web.Application(loop=loop)
-    app.router.add_route('GET', '/', index)
-    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
-    logging.info('server started at http://127.0.0.1:9000...')
-    return srv
+async def echo(request):
+    echo1 = request.match_info.get('echo1', "None")
+    echo2 = request.match_info.get('echo2', "None")
+    text = "<html><body><p>Hello, </p><h1>echo1 is " + echo1 + "</h1><h2>echo2 is " + echo2 + "</h2></body></html>"
+    ###如果WEB页面都这样写，就没法维护了，所以还得靠模板
+    return web.Response(body=text.encode('utf-8'))
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(init(loop))
-loop.run_forever()
+###下面是https://pypi.python.org/pypi/aiohttp给出的官方例子
+async def wshandler(request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    async for msg in ws:
+        if msg.tp == web.MsgType.text:
+            ws.send_str("Hello, {}".format(msg.data))
+        elif msg.tp == web.MsgType.binary:
+            ws.send_bytes(msg.data)
+        elif msg.tp == web.MsgType.close:
+            break
+
+    return ws
+
+app = web.Application(debug=True)
+app.router.add_route('GET', '/', hellow_world)
+app.router.add_route('GET', '/echo', wshandler)
+app.router.add_route('GET', '/{echo1}/{echo2}', echo)###app.router.add_route妥妥的会被注入啊
+logging.info('server started at http://127.0.0.1:9000...')
+web.run_app(app,host='127.0.0.1',port=9000)

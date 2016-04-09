e27459ac25e387fba423476cc2fa8ff20f05b542
lake.lai <yaopower@vip.qq.com>
2016-04-09 20:39:50 +0800

    修改了查询内容

--- PythonApplication1/爬虫练习/chromedriver.py
+++ PythonApplication1/爬虫练习/chromedriver.py
@@ -12,7 +12,7 @@ def chrome(url):
     return data
 
 items=['HLJ', 'JL', 'LN', 'BJ', 'TJ', 'NMG', 'HB', 'SX', 'SAX', 'QH', 'GS', 'NX', 'XJ', 'SH', 'JS', 'ZJ', 'AH', 'FJ', 'JX', 'SD', 'GD', 'GX', 'HAIN', 'HEN', 'HUB', 'HUN', 'CQ', 'SC', 'GZ', 'YN', 'XZ']
-companies=['%E5%96%84%E6%9E%97%EF%BC%88%E4%B8%8A%E6%B5%B7%EF%BC%89%E5%95%86%E5%8A%A1%E5%92%A8%E8%AF%A2%E6%9C%89%E9%99%90%E5%85%AC%E5%8F%B8','%E5%96%84%E6%9E%97%EF%BC%88%E4%B8%8A%E6%B5%B7%EF%BC%89%E9%87%91%E8%9E%8D%E4%BF%A1%E6%81%AF%E6%9C%8D%E5%8A%A1%E6%9C%89%E9%99%90%E5%85%AC%E5%8F%B8','%E5%96%84%E6%9E%97%EF%BC%88%E4%B8%8A%E6%B5%B7%EF%BC%89%E4%BF%A1%E6%81%AF%E7%A7%91%E6%8A%80%E6%9C%89%E9%99%90%E5%85%AC%E5%8F%B8','%E5%96%84%E6%9E%97%EF%BC%88%E4%B8%8A%E6%B5%B7%EF%BC%89%E5%95%86%E5%8A%A1%E5%92%A8%E8%AF%A2%E6%9C%8D%E5%8A%A1%E6%9C%89%E9%99%90%E5%85%AC%E5%8F%B8']
+companies=['%E5%96%84%E6%9E%97%EF%BC%88%E4%B8%8A%E6%B5%B7','%E5%96%84%E6%9E%97(%E4%B8%8A%E6%B5%B7']
 url='http://qichacha.com/search?key={0}&province={1}&p={2}&index='
 output=[]
 chromedriver ='C:\\Users\\yaopr\\AppData\\Local\\Google\\Chrome\\chromedriver.exe'

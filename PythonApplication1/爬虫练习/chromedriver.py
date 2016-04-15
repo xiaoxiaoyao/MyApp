db0d3f6ac0b1aee3c16291f67ec8b99d0f58a80a
lake.lai <yaopower@vip.qq.com>
2016-04-15 21:13:14 +0800

    没公司就自动停止。不会不停的翻页了

--- PythonApplication1/爬虫练习/chromedriver.py
+++ PythonApplication1/爬虫练习/chromedriver.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
+'''
+我的第一个爬虫
+
+爬自己公司的所有子公司，方便搜索，简单粗暴
+'''
+__author__ = 'lai yao (lake.lai)'
+__date__ ="$2016-04-15 20:56:05$"
 
 import os,time
-fileObj = open('C:\\Users\\yaopr\\AppData\\Local\\Temp\\adcon\\output.txt','w') 
+fileObj = open('C:\\Users\\yaopr\\Source\\Repos\\PythonApplication1\\OUTPUT\\output.txt','w') 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -16,26 +23,36 @@ def chrome(url):
     return data
 
 items=['HLJ', 'JL', 'LN', 'BJ', 'TJ', 'NMG', 'HB', 'SX', 'SAX', 'QH', 'GS', 'NX', 'XJ', 'SH', 'JS', 'ZJ', 'AH', 'FJ', 'JX', 'SD', 'GD', 'GX', 'HAIN', 'HEN', 'HUB', 'HUN', 'CQ', 'SC', 'GZ', 'YN', 'XZ']
-companies=['%E5%96%84%E6%9E%97%EF%BC%88%E4%B8%8A%E6%B5%B7','%E5%96%84%E6%9E%97(%E4%B8%8A%E6%B5%B7']
+companies=['%E5%96%84%E6%9E%97 %E4%B8%8A%E6%B5%B7'] #'%E5%96%84%E6%9E%97%EF%BC%88%E4%B8%8A%E6%B5%B7','
 url='http://qichacha.com/search?key={0}&province={1}&p={2}&index='
 output=[]
 chromedriver ='C:\\Users\\yaopr\\AppData\\Local\\Google\\Chrome\\chromedriver.exe'
 os.environ["webdriver.chrome.driver"] = chromedriver
+print('here we go chrome')
 driver = webdriver.Chrome(chromedriver)
 driver.get('http://qichacha.com/user_login')
-print('here we go chrome')
 for i in range(20):
     print('time.sleep',20-i)
     time.sleep(1)
 
-import re
+import re #(pre-treatment)
 print('url=',url)
 for item in items:
     for company in companies:
-        for i in range(9):
-            output.append([item,company,i,re.findall('善林.*?存续|善林.*?在业',chrome(url.format(company,item,i)))])
-            print(output[-1])
-            time.sleep(2.98) 
+        i=1
+        while True:
+            text=chrome(url.format(company,item,i))
+            time.sleep(2.98)
+            try:
+                re.search('善林',text).group(0)
+            except AttributeError:
+                print('Page=',i,'\nitem=',item,'\ncompany=',company,'\ntext=',text,'\n',AttributeError)#for break
+                break
+            else:
+                output.append(re.findall('善林.*? 存续|善林.*? 在业',text))
+                print(item,company,i,output[-1])
+            finally:
+                i=i+1
 print(output)
 #elem = driver.find_element_by_link_text(nextpage)
 #elem.click()

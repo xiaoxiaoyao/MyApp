f2116bab8423f952c5b960ff11d06a84ab77ebec
lake.lai <yaopower@vip.qq.com>
2016-04-26 19:29:56 +0800

    增加了try结构2
    嗯，大概真的聪明点了。。。。。。

--- PythonApplication1/爬虫练习/README.md
+++ PythonApplication1/爬虫练习/README.md
@@ -1,4 +1,4 @@
-#爬虫
+﻿#爬虫
 
 <br>我自己的python练习册，python尊好玩o(^▽^)o
 
@@ -19,14 +19,15 @@ StatsModels：用于探索数据、估计统计模型、统计检验；
 scikit-learn：提供经典的机器学习算法用于数据挖掘和数据分析；
 matplotlib： 2D绘图库，可绘制高质量的图片。
 
-pip install six
-pip install python_dateutil
-pip install pytz
-pip install pandas
-pip install patsy
-pip install scipy
-pip install statsmodels
-pip install scikit_learn
-pip install pyparsing
-pip install cycler
-pip install matplotlib
+pip install re --upgrade
+pip install six --upgrade
+pip install python_dateutil --upgrade
+pip install pytz --upgrade
+pip install pandas --upgrade
+pip install patsy --upgrade
+pip install scipy --upgrade
+pip install statsmodels --upgrade
+pip install scikit_learn --upgrade
+pip install pyparsing --upgrade
+pip install cycler --upgrade
+pip install matplotlib --upgrade
--- PythonApplication1/爬虫练习/chromedriver.py
+++ PythonApplication1/爬虫练习/chromedriver.py
@@ -16,27 +16,28 @@ from selenium import webdriver
 ##import asyncio
 ##loop = asyncio.get_event_loop()
 
-def chrome(url):
+def chrome(url,j=1):
     print('def chrome(url):')
     driver.get(url)
     data=[]
-    i=1
+    j=1
     while True:
         try:
             driver.switch_to_window(driver.window_handles[0])
-            data.append(driver.find_element_by_xpath(str('//*[@id="searchlist"]['+str(i)+']')).text)
+            data.append(driver.find_element_by_xpath(str('//*[@id="searchlist"]['+str(j)+']')).text)
             fileObj2.write(data[-1])
             driver.switch_to_window(driver.window_handles[0])
         except BaseException as err:
-            print(i,err)
+            print(j,err)
             return str(data)
             break
         else:
-            print(i,'(found)',data[i-1])
-            qichacha_update(driver,i)#顺带更新一下数据
-            ##loop.run_until_complete(qichacha_update(driver,i))
-            i=i+1
+            print(j,'(found)',data[j-1])
+            qichacha_update(driver,j)#顺带更新一下数据
+            ##loop.run_until_complete(qichacha_update(driver,j))
+            j=j+1
         finally:
+            driver.switch_to_window(dri.window_handles[0])
             pass
 
 def qichacha_update(dri=None,i=1):
@@ -50,19 +51,26 @@ def qichacha_update(dri=None,i=1):
         return  #页面开了没？
     dri.switch_to_window(dri.window_handles[len(dri.window_handles)-1])
     time.sleep(3)
-    dri.find_element_by_xpath('//*[@id="company-top"]/div/div[2]/a[3]').click()
-    time.sleep(1)
-    dri.find_element_by_xpath('//*[@id="company-top"]/div/div[2]/a[3]/i').click()
-    dri.switch_to_window(dri.window_handles[0])
-    time.sleep(7)
-    dri.switch_to_window(dri.window_handles[len(dri.window_handles)-1])#切换到最后一个标签页
-    dri.close()
-    dri.switch_to_window(dri.window_handles[0])
-    print(dri.window_handles,'dri.switch_to_window(dri.window_handles[0])')#for debug
+    try:
+        dri.find_element_by_xpath('//*[@id="company-top"]/div/div[2]/a[3]').click()
+        time.sleep(1)
+        dri.find_element_by_xpath('//*[@id="company-top"]/div/div[2]/a[3]/i').click()
+        dri.switch_to_window(dri.window_handles[0])
+    except BaseException as err:
+        print(i,err)
+        print('no found update date 哎呀没点到更新数据')
+        return
+    else:
+        time.sleep(7)
+        dri.switch_to_window(dri.window_handles[len(dri.window_handles)-1])#切换到最后一个标签页
+        dri.close()
+    finally:
+        dri.switch_to_window(dri.window_handles[0])
+        print(dri.window_handles,'dri.switch_to_window(dri.window_handles[0])')#for debug
     pass
 
-
-items=['HLJ', 'JL', 'LN', 'BJ', 'TJ', 'NMG', 'HB', 'SX', 'SAX', 'QH', 'GS', 'NX', 'XJ', 'SH', 'JS', 'ZJ', 'AH', 'FJ', 'JX', 'SD', 'GD', 'GX', 'HAIN', 'HEN', 'HUB', 'HUN', 'CQ', 'SC', 'GZ', 'YN', 'XZ']
+##初始化##
+items=['SX', 'NMG', 'HB', 'SAX', 'HLJ', 'JL', 'LN', 'BJ', 'TJ', 'QH', 'GS', 'NX', 'XJ', 'SH', 'JS', 'ZJ', 'AH', 'FJ', 'JX', 'SD', 'GD', 'GX', 'HAIN', 'HEN', 'HUB', 'HUN', 'CQ', 'SC', 'GZ', 'YN', 'XZ']
 companies=['%E5%96%84%E6%9E%97 %E4%B8%8A%E6%B5%B7'] #'%E5%96%84%E6%9E%97%EF%BC%88%E4%B8%8A%E6%B5%B7','
 url='http://qichacha.com/search?key={0}&province={1}&p={2}&index='
 output=[]
@@ -73,35 +81,48 @@ option.add_argument('--user-data-dir=C:\\Users\\yaopr\\AppData\\Local\\Google\\C
 option.add_argument('--user-agent=iphone') #修改浏览器的User-Agent来伪装你的浏览器访问手机m站
 option.add_argument('--process-per-site') #每个站点使用单独进程
 option.add_argument('--lang=zh-CN') #设置语言为简体中文
-print('here we go chrome')
+##完成初始化##
+print('##完成初始化## Here we go chrome')
 driver = webdriver.Chrome(chromedriver,chrome_options=option)
 driver.get('about:version')
-time.sleep(1)
+time.sleep(2)
+print('##成功开启chrome##')
 driver.get('http://qichacha.com/user_login')
-for i in range(10):
-    print('time.sleep',10-i)
-    time.sleep(1)
+time.sleep(1)
 
 import re #(pre-treatment)
 print('url=',url)
+i=1
+company=companies[0]
 for item in items:
-    for company in companies:
-        i=1
-        while True:
-            print('\n\n\n\n\n\ntext=chrome(url.format(company,item,i))')
+    while i != -1:
+        print('\n\ntext=chrome(url.format(company,item,i)) url.format(company,item,i)=',url.format(company,item,i))
+        try:
             text=chrome(url.format(company,item,i))
-            time.sleep(2.98)
-            try:
-                re.search('善林',text).group(0)
-            except AttributeError:
-                print('Page=',i,'\nitem=',item,'\ncompany=',company,'\ntext=',text,'\n',AttributeError)#for break
-                break
-            else:
-                output.append(re.findall('善林.*? 存续|善林.*? 在业',text))
-                fileObj.write(str(output[-1]))
-                print(item,company,i,driver.current_url,output[-1])
-            finally:
-                i=i+1
+            time.sleep(2.9)
+            re.search('善林',text).group(0)
+        except AttributeError:
+            print('Page=',i,'\nitem=',item,'\ncompany=',company,'\ntext=',text,'\n',AttributeError)#for break
+            i=1
+            break
+        except KeyboardInterrupt:
+            print('KeyboardInterrupt',KeyboardInterrupt)
+            fileObj.close()
+            fileObj2.close()
+            driver.close()
+            driver.quit()
+            i=-1#for quit
+            break
+        except BaseException as err:
+            print('except BaseException as err:',err)
+        else:
+            output.append(re.findall('善林.*? 存续|善林.*? 在业',text))
+            fileObj.write(str(output[-1]))
+            fileObj.write('\n')
+            print(item,company,i,driver.current_url,output[-1])
+            i=i+1
+        finally:
+            print('try-finally : Page=',i,'item=',item,'company=',company,'text=',text,'\n')
 print(output)
 #elem = driver.find_element_by_link_text(nextpage)
 #elem.click()

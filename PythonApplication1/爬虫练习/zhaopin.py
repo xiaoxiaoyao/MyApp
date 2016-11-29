9de8ab533e384e121638161a854014bc6f55bc0e
lake.lai <yaopower@vip.qq.com>
2016-11-30 01:16:46 +0800

    智联招聘自动投简历程序
    完善了一下，更方便更好用了呢~~
    之前完善了日志记录文件的逻辑，再也不会找不到文件了

--- PythonApplication1/爬虫练习/zhaopin.py
+++ PythonApplication1/爬虫练习/zhaopin.py
@@ -7,7 +7,7 @@
 if __name__=='__main__':
 	timeout=10
 	KeyWordUrl='http://sou.zhaopin.com/jobs/searchresult.ashx?kw=%E7%A8%8E&sm=0&bj=2060000&isfilter=1&p=1&sf=8001&st=8000'
-	KeyWordUrl='http://sou.zhaopin.com/jobs/searchresult.ashx?kw=%E7%A8%8E&sm=0&bj=2060000&isfilter=1&p=1&sf=8001&st=10000'
+	#KeyWordUrl='http://sou.zhaopin.com/jobs/searchresult.ashx?kw=%E7%A8%8E&sm=0&bj=2060000&isfilter=1&p=1&sf=8001&st=10000'
 	##/初始化路径
 	import os,sys,time
 	try:
@@ -44,25 +44,36 @@ if __name__=='__main__':
 	##启动浏览器
 	#driver = webdriver.Chrome(chromedriver,chrome_options=option)
 	driver = webdriver.Chrome(chromedriver)
-	driver.get('http://sou.zhaopin.com/jobs/searchresult.ashx?kw=%E7%A8%8E&sm=0&p=1')
 	driver.get('http://www.zhaopin.com/?about:version')
+	time.sleep(1)
+	n=driver.find_element_by_xpath('//*[@id="loginname"]')
+	n.send_keys("13681776437")
 	print('#给你60秒输入用户名密码你懂得')#给你输入用户名密码你懂得
 	for n in range(60):
 		time.sleep(1)
 		print(n+1,'秒')
 	driver.get(KeyWordUrl)#搜索职位关键词
 	driver.switch_to_window(driver.window_handles[len(driver.window_handles)-1])#切换到最后一个标签页
-	for n in range(40):
+	for n in range(25):
+		print(n,'for n in range(25):')
 		time.sleep(timeout)
-		driver.find_element_by_xpath('//*[@id="newlist_list_content_table"]/table['+ str(n+2) + ']/tbody/tr[1]/td[1]/div/a').click()
-		time.sleep(timeout)
-		driver.switch_to_window(driver.window_handles[len(driver.window_handles)-1])#切换到最后一个标签页
-		driver.find_element_by_xpath('//*[@id="applyVacButton1"]').click()
-		time.sleep(timeout)
-		driver.switch_to_window(driver.window_handles[len(driver.window_handles)-1])#切换到最后一个标签页
-		driver.find_element_by_xpath('//*[@id="list_d"]/div[1]/div[1]/button[1]').click()
-		time.sleep(timeout)
-		driver.close()
-		driver.switch_to_window(driver.window_handles[len(driver.window_handles)-1])#切换到最后一个标签页
+		try:
+			driver.find_element_by_xpath('//*[@id="newlist_list_content_table"]/table['+ str(n+2) + ']/tbody/tr[1]/td[1]/div/a').click()
+			time.sleep(timeout)
+			driver.switch_to_window(driver.window_handles[len(driver.window_handles)-1])#切换到最后一个标签页
+			driver.find_element_by_xpath('//*[@id="applyVacButton1"]').click()
+			time.sleep(timeout)
+			driver.switch_to_window(driver.window_handles[len(driver.window_handles)-1])#切换到最后一个标签页
+			driver.find_element_by_xpath('//*[@id="list_d"]/div[1]/div[1]/button[1]').click()
+			time.sleep(timeout)
+		except BaseException as err:
+			print(err)
+			driver.get(KeyWordUrl)#搜索职位关键词
+			driver.switch_to_window(driver.window_handles[0])#切换到第一个标签页
+			driver.get(KeyWordUrl)#搜索职位关键词
+		else:
+			driver.close()
+		finally:
+			driver.switch_to_window(driver.window_handles[0])#切换到第一个标签页
 	driver.close()
 	driver.quit()

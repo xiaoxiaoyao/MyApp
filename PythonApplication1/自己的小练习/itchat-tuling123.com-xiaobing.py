f690fd55003d3dcb6762f51bcf36295b73992218
xiaoxiaoyao <yao.power@qq.com>
2017-08-26 23:57:18 +0800

    修正一个小错误，防止无限给小冰发信息

--- PythonApplication1/自己的小练习/itchat-tuling123.com-xiaobing.py
+++ PythonApplication1/自己的小练习/itchat-tuling123.com-xiaobing.py
@@ -21,14 +21,17 @@ def tuling_reply(msg):
   # a or b的意思是，如果a有内容，那么返回a，否则返回b
   return reply or defaultReply
 
-@itchat.msg_register(itchat.content.TEXT, isFriendChat=True, isGroupChat=True, isMpChat=True)
+# 微信好友发来的内容isFriendChat=True, 群聊发来的内容isGroupChat=True, 公众号发来的内容isMpChat=False
+@itchat.msg_register(itchat.content.TEXT, isFriendChat=True, isGroupChat=True, isMpChat=False)
 def xiaobing(msg):
 	time.sleep(random.random()*5)
+  # 发送图灵机器人回复内容
 	itchat.send(tuling_reply(msg),toUserName=itchat.search_mps(name='小冰')[0]['UserName'])
-	time.sleep(random.random()*3)
-	itchat.send(msg['Content'],toUserName=itchat.search_mps(name='小冰')[0]['UserName'])
-	time.sleep(random.random()*1)
-	
+	#time.sleep(random.random()*3)
+  # 发送之前的内容
+	#itchat.send(msg['Content'],toUserName=itchat.search_mps(name='小冰')[0]['UserName'])
+	#time.sleep(random.random()*1)
+
 KEY = '75137612d89c42f0b9d7a3f5133ec656' #这个key可以直接拿来用，随便用，无所谓，放心公开
 
 if __name__ == '__main__':

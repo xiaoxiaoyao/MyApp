9477bc0c83d4671e530024048919776606fde3fe
lake.lai <yaopower@vip.qq.com>
2017-08-29 11:27:28 +0800

    下班不再忘记打卡
    自动打卡，下班不再忘记打卡

---
+++ Tampermonkey/JD-auto-checkIn.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         JD-auto-checkIn
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  彻底解决下班忘记打卡烦恼，开着页面就每隔3.3分钟自动打一次卡
+// @author       xiaoxiaoyao
+// @match        http://erp.jd.com/
+// @grant        none
+// @supportURL   https://github.com/xiaoxiaoyao/PythonApplication1/issues
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    // Your code here...
+    setInterval(function(){
+        //要执行的代码
+        checkIn();
+    },200000);
+
+    location.reload([bForceGet]);
+})();

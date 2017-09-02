321618a6c65607e5b8b7d1a8eb8565f44e7b1fab
xiaoxiaoyao <yao.power@qq.com>
2017-09-02 17:09:07 +0800

    日常更新

--- PythonApplication1/自己的小练习/itchat-tuling123.com-xiaobing.py
+++ PythonApplication1/自己的小练习/itchat-tuling123.com-xiaobing.py
@@ -1,4 +1,7 @@
 import requests,time,random,itchat
+
+#自动把各种聊天内容发给微软小冰isXiaobingChat的小程序（附带图灵机器人和小冰聊天isTuling）
+isTuling,isXiaobingChat= True ,True
 def get_response(msg):
   Url = 'http://www.tuling123.com/openapi/api'
   data = {
@@ -22,18 +25,19 @@ def tuling_reply(msg):
   return reply or defaultReply
 
 # 微信好友发来的内容isFriendChat=True, 群聊发来的内容isGroupChat=True, 公众号发来的内容isMpChat=False
-@itchat.msg_register(itchat.content.TEXT, isFriendChat=True, isGroupChat=True, isMpChat=False)
+isFriendChat, isGroupChat , isMpChat=True,True, False
+@itchat.msg_register(itchat.content.TEXT, isFriendChat=isFriendChat, isGroupChat=isGroupChat, isMpChat=isMpChat)
 def xiaobing(msg):
-	time.sleep(random.random()*5)
   # 发送图灵机器人回复内容
-	itchat.send(tuling_reply(msg),toUserName=itchat.search_mps(name='小冰')[0]['UserName'])
-	#time.sleep(random.random()*3)
+  if isTuling:
+        time.sleep(random.random()*2)
+        itchat.send(tuling_reply(msg),toUserName=itchat.search_mps(name='小冰')[0]['UserName'])
   # 发送之前的内容
-	#itchat.send(msg['Content'],toUserName=itchat.search_mps(name='小冰')[0]['UserName'])
-	#time.sleep(random.random()*1)
-
-KEY = '75137612d89c42f0b9d7a3f5133ec656' #这个key可以直接拿来用，随便用，无所谓，放心公开
+  if isXiaobingChat:
+        time.sleep(random.random()*3)
+        itchat.send(msg['Content'],toUserName=itchat.search_mps(name='小冰')[0]['UserName'])
 
 if __name__ == '__main__':
-	itchat.auto_login(enableCmdQR=2)
-	itchat.run()
+  KEY = '75137612d89c42f0b9d7a3f5133ec656' #这个key可以直接拿来用，随便用，无所谓，放心公开
+  itchat.auto_login(enableCmdQR=2)
+  itchat.run()

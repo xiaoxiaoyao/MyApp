cef4b283acdf9c432e3ca89484eaaf2ce2646f62
lake.lai <yaopower@vip.qq.com>
2017-10-04 15:26:38 +0800

    中秋快乐-itchat-输出更友好
    The Midautumn Festival is the traditional festival in China.In this day,people ,espically family members will have a happy get-together.So Chinese people vaule this festival for its important meaning of "reunion".And the mooncake is the symbolic food.It represents the "reunion" just like the full moon.
    Chinese Mid-Autumn Festival is one of the four major traditional festivals.中秋节是中国四大传统节日之一。
    There are evening meal, people who work outside the home have to come back happy. After dinner, people lit lanterns, usually red lanterns round. The children will happily play their toy lanterns.
    晚上有一顿美餐，离家在外工作的人也要回来团圆。晚饭后，人们点亮灯笼，一般是红色的圆灯笼。孩子们会高高兴兴地玩他们的玩具灯笼。
    You Yuanyou the moon in the evening, people eating at the same time to celebrate the Mid-Autumn Festival special foods - cakes. People with the past, look to the future. It is said that there are dragon in the sky, it is necessary to swallow the moon. In order to protect the moon, the children have to come up with a large ring of the dragons away.
    晚上月亮又圆又大，人们在赏月的同时吃着中秋节特别的食品——月饼。人们在一起回顾过去，展望未来。据说天上有一条龙，它要把月亮吞下去。为了保护月亮，孩子们要弄出很大的响动把龙吓跑。

--- PythonApplication1/自己的小练习/Happy-Mid-Autumn-Festival.py
+++ PythonApplication1/自己的小练习/Happy-Mid-Autumn-Festival.py
@@ -5,20 +5,21 @@ itchat.auto_login(True)
 i=1
 friendList = itchat.get_friends(update=True)[i:]
 
+print('即将给',len(friendList),'个好友发送中秋祝福，祝福内容为以下随机挑一个：\n',SINCERE_WISH)
 
 for friend in friendList:
+	# 祝福语中随机选一条
+	SEND_WISH=random.choice(SINCERE_WISH)
 	try:
-		# 祝福语中随机选一条
-		n=random.randint(0,len(SINCERE_WISH)-1)
 		# 如果是演示目的，把下面的 itchat.send 方法改为 print 即可
-		itchat.send(SINCERE_WISH[n], friend['UserName'])
-		print(SINCERE_WISH[n], friend['UserName'])
+		itchat.send(SEND_WISH, friend['UserName'])
+		print(friend['UserName'],'\n第',i,'个好友已经成功发送，发送内容为',SEND_WISH)
 		# print(friend['DisplayName'] or friend['NickName'])
-		#为了防止封号
+		# 为了防止封号，自动延时发送
 		time.sleep(1+random.random()*124)
 	except Exception:# 用于解决异常情况
 		print(Exception,":")
-		print('ERROR!')
+		print('ERROR!\n第',i,'个好友发送失败，以下是详细信息')
 		friend
 	finally:
 		# 计数

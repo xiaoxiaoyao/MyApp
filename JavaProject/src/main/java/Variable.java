621cb47838c1665cd51da99066fed84753c623f8
xiaoxiaoyao <yao.power@qq.com>
2017-11-24 23:53:43 +0800

    学点简单的JAVA

---
+++ JavaProject/src/main/java/Variable.java
@@ -0,0 +1,16 @@
+package main.java;
+
+public class Variable {
+	public static final void main(String[] arguments) {
+		Boolean gameOver = false;
+		byte escapeKey = 20;
+		short roomNumber = 202;
+		int tops = 1;
+		long salary = 264_400_000;
+		float gradePointAverage = 3.5F;
+		char key = 'C';
+		final String KEY = "ASDFSADFSADF";
+		String productName = "Larvets";
+		
+	}
+}
---
+++ cpp/first.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   cout<<"hello Ubuntu 14.04!"<<endl;
+   cout<<"hello,大家好！"<<endl;
+   return 0;
+}

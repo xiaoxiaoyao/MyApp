26b64c193c8dd3df737658af17374f6551543210
xiaoxiaoyao <yao.power@qq.com>
2017-12-17 20:41:06 +0800

    创建默认react-app

---
+++ JavaProject/src/main/java/PrimeFinder.java
@@ -0,0 +1,36 @@
+/**  
+ * Project Name:JavaProject  
+ * File Name:PrimeFinder.java  
+ * Package Name:main.java  
+ * Date:2017年12月17日下午8:27:08  
+ * Copyleft (c) 2017, git@github.com:xiaoxiaoyao/MyApp.git .
+ *  
+*/  
+  
+package main.java;  
+/**  
+ * ClassName:PrimeFinder <br/>  
+ * Function: TODO ADD FUNCTION. <br/>  
+ * Reason:   TODO ADD REASON. <br/>  
+ * Date:     2017年12月17日 下午8:27:08 <br/>  
+ * @author   yao  
+ * @version    
+ * @since    JDK 1.6  
+ * @see        
+ */
+public class PrimeFinder implements Runnable {
+
+	public PrimeFinder() {
+
+		// TODO Auto-generated constructor stub  
+
+	}
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub  
+
+	}
+
+}
+  

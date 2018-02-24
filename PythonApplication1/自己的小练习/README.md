a078453ab38df188d996d8d60d12398d687e08cd
xiaoxiaoyao <yao.power@qq.com>
2018-02-25 00:01:05 +0800

    编码调整为UTF-8

--- PythonApplication1/自己的小练习/README.md
+++ PythonApplication1/自己的小练习/README.md
@@ -1,9 +1,9 @@
-#С��ϰ
+#小练习
 
-<br>���Լ���python��ϰ�ᣬpython�����o(^��^)o
+<br>我自己的python练习册，python尊好玩o(^▽^)o
 
-<br>���Ŀ¼������Ÿ���python�Ķ�С��ϰ
+<br>这个目录用来存放各类python的短小练习
 
-<br>�Լ�����ʵ�����python����ֵֹķ����Լ�����ֵֹ��÷�
+<br>以及用于实验各类python奇奇怪怪的方法以及奇奇怪怪的用法
 
-<br>ÿ�춼Ҫ���ģ�������
+<br>每天都要开心，啦啦啦
--- Tampermonkey/JD-auto-checkIn.user.js
+++ Tampermonkey/JD-auto-checkIn.user.js
@@ -2,7 +2,7 @@
 // @name         JD-auto-checkIn
 // @namespace    https://github.com/xiaoxiaoyao/MyApp/
 // @version      0.1.1
-// @description  彻底解决下班忘记打卡烦恼，开着页面就每隔3.3分钟自动打一次卡，注意半夜也会打卡，只要开机就打卡
+// @description  解决下班忘记打卡烦恼，开着页面就每隔3.3分钟自动打一次卡，注意半夜也会打卡，只要开机就打卡
 // @author       xiaoxiaoyao
 // @match        http://jdwe.jd.com/
 // @grant        none

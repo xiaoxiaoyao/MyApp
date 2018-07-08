8d83c1f98dab5e5ffcc3881313a692837bba299b
lake.lai <yaopower@vip.qq.com>
2018-07-08 14:43:41 +0800

    正则表达式加几个常用的URL匹配套路
    # 附上几个网上搜到的验证Url地址的正则表达式
    regex = re.compile(
            r'^(?:http|ftp)s?://' # http:// or https://
            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
            r'localhost|' #localhost...
            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
            r'(?::\d+)?' # optional port
            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
    
    # 关于python使用re模块的第三个参数简单了解
    
    # re.I(re.IGNORECASE): 忽略大小写（括号内是完整写法）
    #        re.M(re.MULTILINE): 多行模式，改变'^'和'$'的行为
    #        re.S(re.DOTALL): 点任意匹配模式，改变'.'的行为

--- PythonApplication1/自己的小练习/regular-expression-operations.py
+++ PythonApplication1/自己的小练习/regular-expression-operations.py
@@ -34,3 +34,18 @@ if match3:
     print(match3.group())
 else:  
     print('match3匹配失败！')
+
+# 附上几个网上搜到的验证Url地址的正则表达式
+regex = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+# 关于python使用re模块的第三个参数简单了解
+
+# re.I(re.IGNORECASE): 忽略大小写（括号内是完整写法）
+#        re.M(re.MULTILINE): 多行模式，改变'^'和'$'的行为
+#        re.S(re.DOTALL): 点任意匹配模式，改变'.'的行为

d2054a1a9adf02b236a437e05172156a85642e95
lake.lai <yaopower@vip.qq.com>
2018-09-11 18:15:05 +0800

    Objective-C，我还是直接弃坑比较好。。。
    公司配的是苹果电脑，想试试 iOS 开发，坑太大，一步一步来

---
+++ Objective-C/MyFirstApp/MyFirstApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstApp
+//
+//  Created by laiyao on 2018/9/11.
+//  Copyright © 2018年 laiyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Objective-C/MyFirstApp/MyFirstApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstApp
+//
+//  Created by laiyao on 2018/9/11.
+//  Copyright © 2018年 laiyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

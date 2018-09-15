5510c7548406a2f55796afe62db0c30041e420df
lake.lai <yaopower@vip.qq.com>
2018-09-15 23:26:37 +0800

    [Python爬虫]通过分析胸罩销售评论
    闲暇之余写了一个爬虫例子。通过爬虫去爬取京东的用户评价，通过分析爬取的数据能得到很多结果，比如，哪一种颜色的胸罩最受女性欢迎，以及中国女性的平均size(仅供参考哦~)

---
+++ PythonApplication1/爬虫练习/JD/JD_bra.py
@@ -0,0 +1,70 @@
+# 申明：该爬虫与本人工作经历、任职公司无关，完全出于个人兴趣。
+
+import requests,re
+# 用来获取商品列表
+def find_product_id(key_word="胸罩",page=3):
+    '''
+    *@Function【查询商品id】    首先我们需要在搜索页面获取商品的id，为下面爬取用户评价提供productId.
+    *@Request: 请求 [in]
+    *   param1  string key_word: key_word为搜索的关键字
+    *@Response：响应 [out]
+    *   param1 int page: page为爬取页数默认爬前3页的商品
+    *@Return：返回值 product_ids = [] :  商品 ID
+    '''
+    jd_url = 'https://search.jd.com/Search'
+    product_ids = []
+    # 爬前3页的商品
+    for i in range(1,page):
+        param = {'keyword': key_word, 'enc': 'utf-8', 'page': i}
+        response = requests.get(jd_url, params=param)
+        # 商品id
+        ids = re.findall('data-pid="(.*?)"', response.text, re.S)
+        product_ids += ids
+    return product_ids
+
+import json,threading
+def get_comment_message(product_id='15752689550',pages=11):
+    '''
+    获取评论内容
+    '''
+    urls = ['https://sclub.jd.com/comment/productPageComments.action?' \
+            'callback=fetchJSON_comment98vv53282&' \
+            'productId={}' \
+            '&score=0&sortType=5&' \
+            'page={}' \
+            '&pageSize=10&isShadowSku=0&rid=0&fold=1'.format(product_id, page) for page in range(1, pages)] # 拼接 URL
+    for url in urls:
+        html = requests.get(url).text.replace('fetchJSON_comment98vv53282(', '').replace(');', '')
+        data = json.loads(html)
+        for comment in data['comments']:
+            yield comment # 惰性计算，满速返回
+
+#  因为每种商品的颜色、尺寸描述上有差异，为了方面统计，我们进行了简单的数据清洗。
+def flush_data(data='肤'): # 一键替换同义词；
+    return {
+        '肤' : '肤色', '黑' : '黑色', '紫' : '紫色', '粉' : '粉色', '蓝' : '蓝色', '白' : '白色', '灰' : '灰色', '槟' : '香槟色', '琥' : '琥珀色', '红' : '红色', 'A' : 'A', 'B' : 'B', 'C' : 'C', 'D' : 'D'
+    }.get(data)
+
+def spider_jd(ids):
+    while ids:
+        # 加锁
+        lock.acquire()
+        # 取出第一个元素
+        id = ids[0]
+        # 将取出的元素从列表中删除，避免重复加载
+        del ids[0]
+        # 释放锁
+        lock.release()
+        # 获取评论内容
+        for i in get_comment_message(id,3):
+            print(i)
+            print('---------')
+
+if __name__=='__main__':
+    lock = threading.Lock()
+    product_ids = find_product_id('胸罩')
+    for i in range(1):
+        # 增加一个获取评论的线程
+        t = threading.Thread(target=spider_jd, args=(find_product_id('胸罩'),))
+        # 启动线程.这里预先使用多线程。
+        t.start()

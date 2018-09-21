cd6138e6a0a8e7a7f4bb1191b47a6d077bb56baa
lake.lai <yaopower@vip.qq.com>
2018-09-21 15:11:58 +0800

    GO
    GO

--- GO/hello.go
+++ GO/hello.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func sqrt(x float64) float64 {
+	z := 0.0
+	for i := 0; i < 1000; i++ {
+		z -= (z*z - x) / (2 * x)
+	}
+	return z
+}
 
 func main() {
 	fmt.Printf("hello, world\n")
+	fmt.Printf("Hello,world. Sqrt(2) = %v\n", sqrt(2))
 }

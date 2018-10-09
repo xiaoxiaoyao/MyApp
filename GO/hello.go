2e32554ceac7ab12c3c2b9c3f273416c958233b6
lake.lai <yaopower@vip.qq.com>
2018-10-09 10:43:06 +0800

    GO 语言入门
    GO 语言入门，入门一下

--- GO/hello.go
+++ GO/hello.go
@@ -13,6 +13,13 @@ func sqrt(x float64) float64 {
 }
 
 func main() {
+	for i := 0; i < 3; i++ {
+		defer fmt.Println(
+			"hello, world. defer=",
+			i,
+			"\n")
+	}
+	defer fmt.Println("defer 语句会将函数推迟到外层函数返回之后执行。\n//推迟调用的函数其参数会立即求值，但直到外层函数返回前该函数都不会被调用。\n//推迟的函数调用会被压入一个栈中。当外层函数返回时，被推迟的函数会按照后进先出的顺序调用。\n")
 	fmt.Printf("hello, world\n")
 	fmt.Printf("Hello,world. Sqrt(2) = %v\n", sqrt(2))
 }

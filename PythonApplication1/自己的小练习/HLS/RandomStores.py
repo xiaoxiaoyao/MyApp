073827635e88b9310b3b6313d9ad0850e6e6d897
lake.lai <yaopower@vip.qq.com>
2020-08-17 10:55:02 +0800

    日常同步一下
    啊啊啊啊啊没什么更新

--- PythonApplication1/自己的小练习/HLS/RandomStores.py
+++ PythonApplication1/自己的小练习/HLS/RandomStores.py
@@ -1,3 +1,4 @@
+
 # 读取数据
 filePath=r'F:\\OneDrive\\华莱士\\Documents\\门店监控项目\\城市-营运-督导-门店.csv'
 
@@ -13,7 +14,7 @@ fl = pandas.read_csv(filePath).astype('str').sample(frac=1,random_state=numpy.ra
 
 # 网页服务器部分：
 ###网页模版
-template1='''<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"><html xmlns="http://www.w3.org/1999/xhtml"><body><a href="../chou/30"><button type="button" class="btn btn-warning">点此按钮随机抽30个</button></a><a href="../all"><button type="button" class="btn btn-warning">点此按钮一键全抽</button></a><div class="container">'''
+template1='''<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"><html xmlns="http://www.w3.org/1999/xhtml"><body><a href="../chou/30"><button type="button" class="btn btn-warning">点此按钮随机抽30个</button></a><a href="../all"><button type="button" class="btn btn-warning">点此按钮一键全抽</button></a><a href="../all-no-repeat-by-day/10"><button type="button" class="btn btn-warning">点此按钮一键全抽城市经理(每10天不重复)</button></a><div class="container">'''
 template2='''</div><link href="http://apps.bdimg.com/libs/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" media="screen"><script src="http://apps.bdimg.com/libs/jquery/2.0.0/jquery.min.js"></script><script src="http://apps.bdimg.com/libs/bootstrap/3.3.0/js/bootstrap.min.js"></script></body></html>'''
 
 ###初始化程序_定义服务器
@@ -23,23 +24,36 @@ app = Flask(__name__)
 @app.route('/chou/<n>', methods=("GET", "POST"))
 def 入魂(n):    return template1 + "当前时间：" + datetime.datetime.now().strftime("%x %X") + 一发入魂(n=int(n)).to_html(classes='table-striped') + template2
 
-@app.route('/all', methods=("GET", "POST"))
+@app.route('/all', methods=("GET", "POST")) #一键全抽城市经理(不去重)
 def 一键全抽城市经理():    return template1 + "当前时间：" + datetime.datetime.now().strftime("%x %X") + 城市经理一键全抽().to_html(classes='table-striped') + template2
+
+@app.route('/all-no-repeat-by-day/<n>', methods=("GET", "POST")) #一键全抽城市经理(每<n>天不重复)
+def 不重复抽城市经理(n):    return "建设中"
 
 # @app.route('/city/<city>/<n>', methods=("GET", "POST"))
 # def by区域(city,n=None):    return template1 + "当前时间：" +  datetime.datetime.now().strftime("%x %X") + 按区域抽(区域=city,n=int(n) if n else None).to_html(classes='table-striped') + template2
 
 @app.route('/', methods=("GET", "POST"))
-def index():return template1 + "当前时间：" + datetime.datetime.now().strftime("%x %X") +  template2
+def index():    return template1 + "当前时间：" + datetime.datetime.now().strftime("%x %X") +  template2
 
 # 抽签开始
 # 中间的.drop_duplicates(subset=['城市经理'],keep='first')是用来保证一个'城市经理'只被抽中一次的
-def 一发入魂(n=None, frac=None, replace=False):    return fl.sample(n=n,frac=frac,replace=replace).drop_duplicates(subset=['城市经理'],keep='first')
-def 抽个区域(n=None, frac=None, replace=False):    return fl['区域'].sample(n=n,frac=frac,replace=replace).drop_duplicates(subset=['城市经理'],keep='first')
-def 按区域抽(区域,n=None, frac=None, replace=False):    return fl[fl['区域']==区域].sample(n=n,frac=frac,replace=replace).drop_duplicates(subset=['城市经理'],keep='first')
-def 按城市经理抽(城市经理,n=None, frac=None, replace=False):    return fl[fl['城市经理']==城市经理].sample(n=n,frac=frac,replace=replace).drop_duplicates(subset=['城市经理'],keep='first')
-def 按督导抽(督导,n=None, frac=None, replace=False):    return fl[fl['督导']==督导].sample(n=n,frac=frac,replace=replace).drop_duplicates(subset=['城市经理'],keep='first')
-def 城市经理一键全抽():    return fl.sample(frac=1,random_state=numpy.random.RandomState()).drop_duplicates(subset=['城市经理'],keep='first')
+def 一发入魂(fl=fl,n=None, frac=None, replace=False):    return fl.sample(n=n,frac=frac,replace=replace).drop_duplicates(subset=['城市经理'],keep='first')
+def 抽个区域(fl=fl,n=None, frac=None, replace=False):    return fl['区域'].sample(n=n,frac=frac,replace=replace).drop_duplicates(subset=['城市经理'],keep='first')
+def 按区域抽(区域,fl=fl,n=None, frac=None, replace=False):    return fl[fl['区域']==区域].sample(n=n,frac=frac,replace=replace).drop_duplicates(subset=['城市经理'],keep='first')
+def 按城市经理抽(城市经理,fl=fl,n=None, frac=None, replace=False):    return fl[fl['城市经理']==城市经理].sample(n=n,frac=frac,replace=replace).drop_duplicates(subset=['城市经理'],keep='first')
+def 按督导抽(督导,n=None, fl=fl,frac=None, replace=False):    return fl[fl['督导']==督导].sample(n=n,frac=frac,replace=replace).drop_duplicates(subset=['城市经理'],keep='first')
+def 城市经理一键全抽(fl=fl):    return fl.sample(frac=1,random_state=numpy.random.RandomState()).drop_duplicates(subset=['城市经理'],keep='first')
+
+#不重复抽取
+不重复抽取的日期=1
+fl_copy=fl
+def 城市经理不重复抽(fl=fl_copy):
+    "建设中"
+    return "建设中"
+    
+
+
 
 # 直接选
 def 选个区域():    return fl['区域'].drop_duplicates().sample(frac=1,random_state=numpy.random.RandomState())
